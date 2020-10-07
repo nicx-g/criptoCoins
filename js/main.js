369791eaace2e8467f5a85fcd1f427c351b9ad47
Nicolás Gomez <nicx_g@hotmail.com>
2020-10-07 00:12:55 -0300

    commit inicial de mi repositorio más ejercicio 1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+var nombre;
+var apellido;
+var edad;
+
+nombre=prompt('Ingresá tu nombre por favor');
+apellido=prompt('Ingresá tu apellido!');
+edad=prompt('y ahora tu edad!');
+
+alert("acá están tus datos:"+"\n"+"Nombre:"+" "+nombre+".\n"+"Apellido:"+" "+apellido+".\n"+"Edad:"+" "+edad+".");
+
+alert("Ahora probemos si no sos un robot!");
+
+}
