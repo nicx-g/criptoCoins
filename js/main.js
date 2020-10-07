620039f9269ff7091373c603255f52ba29905490
Nicolás Gomez <nicx_g@hotmail.com>
2020-10-07 01:08:36 -0300

    agregue un generador de numeros aleatorio

--- js/main.js
+++ js/main.js
@@ -11,9 +11,11 @@ window.onload = function(){
     
     alert("Ahora probemos si no sos un robot!");
     
+    var numeroAleatorio1 = Math.round(Math.random()*10);
+    var numeroAleatorio2 = Math.round(Math.random()*10);
     var calculo;
-    calculo=prompt('¿Cuánto es 3 + 9?');
-    if (calculo == 12){
+    calculo=prompt("¿Cuánto es "+numeroAleatorio1+' + '+numeroAleatorio2+'?');
+    if (calculo == numeroAleatorio1 + numeroAleatorio2){
         alert("Muy bien! pasaste la prueba");
     } else{
         alert("Me parece que sos un robot che");
