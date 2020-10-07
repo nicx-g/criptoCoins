11f0431f0a0b53cb1d3014a14fa8d401053f2560
Nicolás Gomez <nicx_g@hotmail.com>
2020-10-07 00:53:19 -0300

    agregue un if

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
 window.onload = function(){
-var nombre;
-var apellido;
-var edad;
-
-nombre=prompt('Ingresá tu nombre por favor');
-apellido=prompt('Ingresá tu apellido!');
-edad=prompt('y ahora tu edad!');
-
-alert("acá están tus datos:"+"\n"+"Nombre:"+" "+nombre+".\n"+"Apellido:"+" "+apellido+".\n"+"Edad:"+" "+edad+".");
-
-alert("Ahora probemos si no sos un robot!");
-
-}
+    var nombre;
+    var apellido;
+    var edad;
+    
+    nombre=prompt('Ingresá tu nombre por favor');
+    apellido=prompt('Ingresá tu apellido!');
+    edad=prompt('y ahora tu edad!');
+    
+    alert("Acá están tus datos:"+"\n \n"+"Nombre:"+" "+nombre+".\n"+"Apellido:"+" "+apellido+".\n"+"Edad:"+" "+edad+".");
+    
+    alert("Ahora probemos si no sos un robot!");
+    
+    var calculo;
+    calculo=prompt('¿Cuánto es 3 + 9?');
+    if (calculo == 12){
+        alert("Muy bien! pasaste la prueba");
+    } else{
+        alert("Me parece que sos un robot che");
+    }
+    
+    };
