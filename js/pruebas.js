a87e56537f903fe7d2790a13870d4322bdd405ad
Nicolás Gomez <nicx_g@hotmail.com>
2020-10-16 01:05:52 -0300

    bueno al final del ejercicio no hice nada pero ya idee más o menos cómo va a ser la web

---
+++ js/pruebas.js
@@ -0,0 +1,10 @@
+let primerNumero = parseInt(prompt("Ingresá tu primer número"));
+let segundoNumero = parseInt(prompt("Ingresá tu segundo número"));
+
+function SumarNumerosYPromediar(numeroUno, numeroDos){
+    let laSumaes = numeroUno + numeroDos;
+    return Promedio = laSumaes / 2;
+}
+
+Promedio1 = SumarNumerosYPromediar(primerNumero, segundoNumero)
+alert(`Hola! el promedio de tu suma es ${Promedio1}`);
