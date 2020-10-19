f8f2819b0bd94d85c70bce7a19202aa4a6f30b8e
Nicolás Gomez <nicx_g@hotmail.com>
2020-10-18 21:53:15 -0300

    esto creo que es lo más hermoso que hice en mi vida hasta ahora. btw: hice la mitad de la primer sección

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,10 +1,13 @@
-let primerNumero = parseInt(prompt("Ingresá tu primer número"));
-let segundoNumero = parseInt(prompt("Ingresá tu segundo número"));
+// let primerNumero = parseInt(prompt("Ingresá tu primer número"));
+// let segundoNumero = parseInt(prompt("Ingresá tu segundo número"));
 
-function SumarNumerosYPromediar(numeroUno, numeroDos){
-    let laSumaes = numeroUno + numeroDos;
-    return Promedio = laSumaes / 2;
-}
+// function SumarNumerosYPromediar(numeroUno, numeroDos){
+//     let laSumaes = numeroUno + numeroDos;
+//     return Promedio = laSumaes / 2;
+// }
 
-Promedio1 = SumarNumerosYPromediar(primerNumero, segundoNumero)
-alert(`Hola! el promedio de tu suma es ${Promedio1}`);
+// Promedio1 = SumarNumerosYPromediar(primerNumero, segundoNumero)
+// alert(`Hola! el promedio de tu suma es ${Promedio1}`);
+
+
+let 
