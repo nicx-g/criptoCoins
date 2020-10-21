1a5ba4d5cbf68e04ea1d96838386d5dcfe2c8257
Nicolás Gomez <nicx_g@hotmail.com>
2020-10-20 21:56:30 -0300

    ejercicio 3

--- js/pruebas.js
+++ js/pruebas.js
@@ -10,4 +10,28 @@
 // alert(`Hola! el promedio de tu suma es ${Promedio1}`);
 
 
-let 
+
+
+let diasParaEscabiar = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+function sePuedeEscabiar() {
+    for (i = 0; i < diasParaEscabiar.length; i ++) {
+       switch (diasParaEscabiar[i]) {
+         case "Martes":
+              console.log(`Hoy ${diasParaEscabiar[i]} podríamos cortar semana,,`);
+              break;
+         case "Viernes":
+         case "Sábado":
+            console.log(`Hoy ${diasParaEscabiar[i]} es un día ideal para escabiar`);
+            break;
+         case "Domingo":
+             console.log(`Hoy ${diasParaEscabiar[i]} hay que descansar de la resaca...o seguir escabiando`);
+           break;
+         default:
+           console.log(`Hoy ${diasParaEscabiar[i]} hay que trabajar! no podemos escabiar! :( ,,o sí`);
+               break;
+       }
+        }
+    }
+
+sePuedeEscabiar();
